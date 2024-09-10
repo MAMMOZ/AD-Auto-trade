b2961d92133a18b38a37db7237e129dacd930c96
MAMMOZ <mosgamedifficul@gmail.com>
2024-09-11 00:32:50 +0700

    a

--- xenon.lua
+++ xenon.lua
@@ -29,37 +29,24 @@ pcall(function()
                         if pages.Enabled then
                             print("PAGES is enabled")
 
-                            getgenv().Configuration = {
-                                ['Enabled'] = true,
-                                ['Enabled Challenge'] = false,
-                                ['PC Name'] = 'dekonemillionbaht',
-                                ['Delay'] = 0,
-                                ['Leave Delay'] = 0,
-                                ['EquipBest'] = true,
-                                ['Leave At Wave'] = 31,
-                                ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
-                                ['TradingMode'] = false,
-                                ['ClaimBattlepass'] = true,
-                                ['Roll Mythic'] = false,
-                                ['Roll Method'] = 1,
-                                ['Auto Feed Mythic'] = false,
-                                ['Use Auto Sell'] = false,
-                                ['Game Speed'] = {
-                                    Enabled = true,
-                                    Speed = 2, -- 1 , 2 ,3
-                                },
-                                ['Sell Config'] = {
-                                    ['Rare'] = false,
-                                    ['Epic'] = false,
-                                    ['Legendary'] = false,
-                                    ['Rare Shiny'] = false,
-                                    ['Epic Shiny'] = false,
-                                    ['Legendary Shiny'] = false,
-                                },
-                                ['Placement Distance'] = 25,
-                            };
-                            getgenv().key = 'bc488106-f328-491d-ae68-e015a4475fe3'
-                            loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
+                            -- repeat task.wait() until game:IsLoaded()
+                            -- wait(5)
+                            _G.Webhook  = {['WebhookLink'] = 'Link', ['SendWebhookReward'] = true}
+                            _G.SettingsAD = {
+                                ["LeaveAtWave"] = 31,
+                                ["DelayStartLobby"] = 20,
+                                ["MethodLeave"] = "Sell",
+                                ["ReplayInStoryMode"] = false,
+                                ["BlackScreen"] = true,
+                                ["EquipBest"] = true,
+                                ["SpeedModifier"] = 1
+                            }
+
+                            getgenv().Key = "MARU-ADIG-JM3XH-PQI7-82ZVQ-MWMB"
+                            getgenv().id = "969581952531316827"
+                            getgenv().Script_Mode = "Kaitun_Script"
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+                            
                         end
                     else
                         local targetPlaceId = 17017769292
