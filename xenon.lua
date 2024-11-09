a71b80804f61724383b99830d3ef3be79171519a
MAMMOZ <38729972+MAMMOZ@users.noreply.github.com>
2024-11-09 16:02:22 +0700

    Update xenon.lua

--- xenon.lua
+++ xenon.lua
@@ -1,50 +1,50 @@
-getgenv().Configuration = {
-    ['Enabled'] = true,
-    ['Enabled Challenge'] = false,
-    ['PC Name'] = 'dekonemillionbaht',
-    ['Delay'] = 0,
-    ['Leave Delay'] = 0,
-    ['EquipBest'] = true,
-    ['Leave At Wave'] = 31,
-    ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
-    ['TradingMode'] = false,
-    ['ClaimBattlepass'] = true,
-    ['Roll Mythic'] = false,
-    ['Roll Method'] = 1,
-    ['Auto Feed Mythic'] = false,
-    ['Use Auto Sell'] = false,
-    ['Game Speed'] = {
-        Enabled = true,
-        Speed = 1, -- 1 , 2 ,3
-    },
-    ['Sell Config'] = {
-        ['Rare'] = false,
-        ['Epic'] = false,
-        ['Legendary'] = false,
-        ['Rare Shiny'] = false,
-        ['Epic Shiny'] = false,
-        ['Legendary Shiny'] = false,
-    },
-    ['Placement Distance'] = 25,
-};
-getgenv().key = '1a9a8b51-c3ee-4c5c-bb21-36f4ee05a56c'
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
+-- getgenv().Configuration = {
+--     ['Enabled'] = true,
+--     ['Enabled Challenge'] = false,
+--     ['PC Name'] = 'dekonemillionbaht',
+--     ['Delay'] = 0,
+--     ['Leave Delay'] = 0,
+--     ['EquipBest'] = true,
+--     ['Leave At Wave'] = 31,
+--     ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
+--     ['TradingMode'] = false,
+--     ['ClaimBattlepass'] = true,
+--     ['Roll Mythic'] = false,
+--     ['Roll Method'] = 1,
+--     ['Auto Feed Mythic'] = false,
+--     ['Use Auto Sell'] = false,
+--     ['Game Speed'] = {
+--         Enabled = true,
+--         Speed = 1, -- 1 , 2 ,3
+--     },
+--     ['Sell Config'] = {
+--         ['Rare'] = false,
+--         ['Epic'] = false,
+--         ['Legendary'] = false,
+--         ['Rare Shiny'] = false,
+--         ['Epic Shiny'] = false,
+--         ['Legendary Shiny'] = false,
+--     },
+--     ['Placement Distance'] = 25,
+-- };
+-- getgenv().key = '1a9a8b51-c3ee-4c5c-bb21-36f4ee05a56c'
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
 
 
--- repeat task.wait() until game:IsLoaded()
--- wait(5)
--- _G.Webhook  = {['WebhookLink'] = 'Link', ['SendWebhookReward'] = true}
--- _G.SettingsAD = {
---     ["LeaveAtWave"] = 31,
---     ["DelayStartLobby"] = 20,
---     ["MethodLeave"] = "Sell",
---     ["ReplayInStoryMode"] = false,
---     ["BlackScreen"] = true,
---     ["EquipBest"] = true,
---     ["SpeedModifier"] = 1
--- }
+repeat task.wait() until game:IsLoaded()
+wait(5)
+_G.Webhook  = {['WebhookLink'] = 'Link', ['SendWebhookReward'] = true}
+_G.SettingsAD = {
+    ["LeaveAtWave"] = 31,
+    ["DelayStartLobby"] = 20,
+    ["MethodLeave"] = "Sell",
+    ["ReplayInStoryMode"] = false,
+    ["BlackScreen"] = true,
+    ["EquipBest"] = true,
+    ["SpeedModifier"] = 1
+}
 
--- getgenv().Key = "MARU-ADIG-JM3XH-PQI7-82ZVQ-MWMB"
--- getgenv().id = "969581952531316827"
--- getgenv().Script_Mode = "Kaitun_Script"
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+getgenv().Key = "MARU-ADIG-JM3XH-PQI7-82ZVQ-MWMB"
+getgenv().id = "969581952531316827"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
