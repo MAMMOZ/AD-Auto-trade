bd6c68ebfe46a09bbfdb842eff145b9069c8d1af
MAMMOZ <mosgamedifficul@gmail.com>
2024-11-14 00:13:38 +0700

    xenon

--- xenon.lua
+++ xenon.lua
@@ -30,6 +30,25 @@ getgenv().Configuration = {
 getgenv().key = '1a9a8b51-c3ee-4c5c-bb21-36f4ee05a56c'
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
 
+-- repeat task.wait() until game:IsLoaded()
+-- wait(5)
+-- _G.Webhook  = {['WebhookLink'] = 'Link', ['SendWebhookReward'] = true}
+-- _G.SettingsAD = {
+--     ["LeaveAtWave"] = 31,
+--     ["DelayStartLobby"] = 20,
+--     ["MethodLeave"] = "Sell",
+--     ["ReplayInStoryMode"] = false,
+--     ["BlackScreen"] = true,
+--     ["EquipBest"] = true,
+--     ["SpeedModifier"] = 1
+-- }
+
+-- getgenv().Key = "MARU-ADIG-JM3XH-PQI7-82ZVQ-MWMB"
+-- getgenv().id = "969581952531316827"
+-- getgenv().Script_Mode = "Kaitun_Script"
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+
+
 wait(60*3)
 
 local lobbyCount = 0 
@@ -48,21 +67,3 @@ while true do
     if lobbyCount >= maxLobbyCount then
         player:Kick("BUG SEVER by Sanbux")
     end
-
--- repeat task.wait() until game:IsLoaded()
--- wait(5)
--- _G.Webhook  = {['WebhookLink'] = 'Link', ['SendWebhookReward'] = true}
--- _G.SettingsAD = {
---     ["LeaveAtWave"] = 31,
---     ["DelayStartLobby"] = 20,
---     ["MethodLeave"] = "Sell",
---     ["ReplayInStoryMode"] = false,
---     ["BlackScreen"] = true,
---     ["EquipBest"] = true,
---     ["SpeedModifier"] = 1
--- }
-
--- getgenv().Key = "MARU-ADIG-JM3XH-PQI7-82ZVQ-MWMB"
--- getgenv().id = "969581952531316827"
--- getgenv().Script_Mode = "Kaitun_Script"
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
