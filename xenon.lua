02b8aca5faad31cb8975e5254e26ff5dd0a184b7
MAMMOZ <38729972+MAMMOZ@users.noreply.github.com>
2024-11-15 13:45:01 +0700

    Update xenon.lua

--- xenon.lua
+++ xenon.lua
@@ -1,18 +1,34 @@
-_G.Webhook  = {['WebhookLink'] = 'Link', ['SendWebhookReward'] = true}
-_G.SettingsAD = {
-    ["LeaveAtWave"] = 31,
-    ["DelayStartLobby"] = 20,
-    ["MethodLeave"] = "Sell",
-    ["ReplayInStoryMode"] = false,
-    ["BlackScreen"] = true,
-    ["EquipBest"] = true,
-    ["SpeedModifier"] = 1
-}
-
-getgenv().Key = "MARU-Q2P5-69DAP-W3ZU-NBQMT-1YP4"
-getgenv().id = "1085195490976616470"
-getgenv().Script_Mode = "Kaitun_Script"
-
+getgenv().Configuration = {
+    ['Enabled'] = true,
+    ['Enabled Challenge'] = false,
+    ['PC Name'] = 'dekonemillionbaht',
+    ['Delay'] = 0,
+    ['Leave Delay'] = 0,
+    ['EquipBest'] = true,
+    ['Leave At Wave'] = 31,
+    ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
+    ['TradingMode'] = false,
+    ['ClaimBattlepass'] = true,
+    ['Roll Mythic'] = false,
+    ['Roll Method'] = 1,
+    ['Auto Feed Mythic'] = false,
+    ['Use Auto Sell'] = false,
+    ['Game Speed'] = {
+        Enabled = true,
+        Speed = 1, -- 1 , 2 ,3
+    },
+    ['Sell Config'] = {
+        ['Rare'] = false,
+        ['Epic'] = false,
+        ['Legendary'] = false,
+        ['Rare Shiny'] = false,
+        ['Epic Shiny'] = false,
+        ['Legendary Shiny'] = false,
+    },
+    ['Placement Distance'] = 25,
+};
+getgenv().key = '1a9a8b51-c3ee-4c5c-bb21-36f4ee05a56c'
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()  
 
 -- local names = {
 --     "yE6LGF", "Zu0tJ8", "mbaOYx", "XkYnAu", "VSiria4", "yt0ROn", "IFVYy2",
