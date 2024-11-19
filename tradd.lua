6da9626285edc4a6f49b02cb6e9e122e3319c443
MAMMOZ <38729972+MAMMOZ@users.noreply.github.com>
2024-11-20 01:11:43 +0700

    Update tradd.lua

--- tradd.lua
+++ tradd.lua
@@ -945,6 +945,7 @@ pcall(function()
                                                     letfkinggo(p63.GAME_MODE_SELECTED_CTS, "TradingLobby")
                                                 end
                                                 while true do
+                                                    print("send anit")
                                                     if plr.Name == getgenv().botsend_anti then
                                                         for _, player in pairs(game:GetService("Players"):GetPlayers()) do
                                                             if isInTable(player.Name, botall) then
